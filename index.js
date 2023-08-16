d96e1c064840a93c1a6e19c6fc6ecf6738755d84
joaoleandro <new.joaoleandro@gmail.com>
2023-08-16 11:03:02 -0300

    adicionando outras informações ao res

--- index.js
+++ index.js
@@ -12,7 +12,60 @@ server.get('/curso', (req, res) => {
 
     // return res.send("Hello World!")
 
-    return res.json({ dados: 'dados da aplicação' })
+    return res.json({ 
+        "drops": [
+            {
+                "id": 1,
+                "name": "Axe of Raah",
+                "monsters": ["Sikuku Elite Fighter", "Venerable Snapper"]
+            },
+            {
+                "id": 2,
+                "name": "Black Monster Axe",
+                "monsters": ["Ramesses Robo Guard", "Nakhtmin Zoombi Guard", "Poisontail Scorpio King", "Prehistoric Mastyx King", "Fearsome Terrasaurus King"]
+            },
+            {
+                "id": 3,
+                "name": "Blade Hunter Sword",
+                "monsters": ["Nakhtmin Zoombi Guard", "Giant Fiery Snapper", "Poisontail Scorpio King", "Prehistoric Mastyx King", "Fearsome Terrasaurus King"]
+            },
+            {
+                "id": 4,
+                "name": "Bloom Wand",
+                "monsters": ["Item Removido do Jogo. :("]
+            },
+            {
+                "id": 5,
+                "name": "Blue Iron Bow",
+                "monsters": ["Saboten", "Rakapuska", "Poisontail Scorpio King", "Prehistoric Mastyx King", "Fearsome Terrasaurus King"]
+            },
+            {
+                "id": 6,
+                "name": "Chimp Napsack(Backpack)",
+                "monsters": ["Doonga Leader"]
+            },
+            {
+                "id": 7,
+                "name": "Crescent Moon",
+                "monsters": ["Cursed Ant Vagabond"]
+            },
+            {
+                "id": 8,
+                "name": "Cross Bones",
+                "monsters": ["Cursed Ant Vagabond"]
+            },
+            {
+                "id": 9,
+                "name": "Dual Samurai",
+                "monsters": ["Ramesses Robo Hunter", "Nakhtmin Zoombi Hunter"]
+            },
+            {
+                "id": 10,
+                "name": "Emperor Plate Mail",
+                "monsters": ["Saboten", "Nakhtmin Zoombi Hunte"]
+            }
+        ]
+    })
 
 
 
